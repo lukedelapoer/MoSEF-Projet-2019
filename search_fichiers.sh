92162436a2107faa16eae149466f304fb3313454
Lucas <lucaspuchetti@gmail.com>
2019-11-28 00:35:33 +0100

    Final version of the search script

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -10,6 +10,10 @@ ls $mydir | less
 #### First Modification
 # Assigning script variables to improve readability
 filename_re=$1
-echo "These are the files containing $filename_re"
-find -name "$filename_re"
+#echo "These are the files containing $filename_re"
+#find -name "$filename_re"
+#### Second Modification
+content_re=$2
+echo "These are the files like $filename_re with a line like $content_re in it."
+find ~ -name "$filename_re" -exec grep -EiH "$content_re" {} \;
 echo "Thank you $USER for using Directory Finder! "
